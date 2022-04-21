addf1a5473eff46ade08cdf61167d6355bd7d3b9
FionnKuo <>
2022-04-21 20:09:36 +0800

    Add modules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
+dotenv.config();
+const address = process.env.IP
 const address = process.env.IP
 
 const main = express()
