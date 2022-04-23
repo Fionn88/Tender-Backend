e6884d5edfce6b4fce9c9673c9246a77ce9c6500
FionnKuo <>
2022-04-23 19:26:55 +0800

    Debug CROS

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const main = express()
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: true }));
 
+main.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -161,4 +169,5 @@ main.post('/createCertificate', async (req, res) => {
   })
 
 })
+
 
